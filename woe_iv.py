3c07718fcefded83b927e0d494ee677c9bcf0b13
starsummer12 <yuanrui0912@yeah.net>
2021-02-03 19:15:04 +0800

    woe_iv psi_re_result

--- woe_iv.py
+++ woe_iv.py
@@ -5,7 +5,7 @@ import  math
 df=pd.read_table("D:\\data_toyuanrui.txt")
 df1=df.T
 collist=list(df.columns[2:])
-df.to_csv("D:\\df.csv")
+# df.to_csv("D:\\df.csv")
 
 def woe_result(df,collist,bin=10):
     minlist=[]
@@ -15,22 +15,23 @@ def woe_result(df,collist,bin=10):
     woelist=[]
     ivlist=[]
     featurelist=[]
-    df = df.replace([np.inf, -np.inf], np.nan).fillna(-99999)
+    df = df.replace([np.inf, -np.inf], np.nan).fillna(-99999999)
     for i in collist:
         # print(i)
-        # i='feature1'
+        # i='feature36'
         pos=df[df['label'] == 1][i].count()
         neg=df[df['label'] == 0][i].count()
         # print(pos,neg)
         quantilest=list(set(df[i].quantile(np.arange(0, 1.000001, 1 / bin))))
         quantilest.sort()
-        print(quantilest)
+        print("i:",i)
+        print("quantilest",quantilest)
         woe=0
         iv=0
         for j in range(len(quantilest)):
             if(j==0):
-                if(quantilest[0]==-99999):
-                    range1= (df[i] == -99999)
+                if(quantilest[0]==-99999999):
+                    range1= (df[i] == -99999999)
                     range_min = df[range1][i].min()
                     range_max = df[range1][i].max()
                     posnum=df[range1 & (df['label'] ==1)][i].count()
@@ -45,7 +46,9 @@ def woe_result(df,collist,bin=10):
                         woe_value=np.log(null_pos_per/null_neg_per)
                         iv_value=woe_value*(null_pos_per-null_neg_per)
                     woe=woe_value
-                    iv+=iv_value
+                    iv=iv_value
+                    # print(iv)
+                    # print("\n")
 
                     featurelist.append(i)
                     minlist.append(range_min)
@@ -56,7 +59,7 @@ def woe_result(df,collist,bin=10):
                     ivlist.append(iv)
 
             else:
-                if (j == 1) & (quantilest[0] != -99999):
+                if (j == 1) & (quantilest[0] != -99999999):
                     min=(df[i] >= quantilest[j-1])
                     max=(df[i] <= quantilest[j])
                     range_min=df[min][i].min()
@@ -69,12 +72,14 @@ def woe_result(df,collist,bin=10):
                     # negnum = neg - posnum
                     null_pos_per = posnum/pos
                     null_neg_per = negnum/neg
-
                 else:
                     min = (df[i] > quantilest[j - 1])
                     max = (df[i] <= quantilest[j])
                     range_min = df[min][i].min()
                     range_max = df[max][i].max()
+
+                    # print(range_min,range_max)
+
                     range1 = (df[i] > quantilest[j-1]) & (df[i] <= quantilest[j])
                     posnum= df[range1 & (df['label'] == 1)][i].count()
                     negnum = df[range1 & (df['label'] == 0)][i].count()
@@ -89,7 +94,11 @@ def woe_result(df,collist,bin=10):
                     woe_value = np.log(null_pos_per / null_neg_per)
                     iv_value = woe_value * (null_pos_per - null_neg_per)
                     woe = woe_value
-                    iv += iv_value
+                    iv = iv_value
+
+                    # print("z",iv)
+                    # print("\n")
+
                 featurelist.append(i)
                 minlist.append(range_min)
                 maxlist.append(range_max)
